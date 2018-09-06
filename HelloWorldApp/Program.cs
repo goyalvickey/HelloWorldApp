80095ddfa024e1b2334283ed0bdbe6297e7f1c6f
innovativeinstitution <innovativeinstitution@gmail.com>
2018-09-05 22:31:17 -0700

    trigger new build

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Universe this is Manish Narayan!");
+            Console.Read();
         }
     }
 }
